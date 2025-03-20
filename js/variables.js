28389fd8e282e5e3c03965041ffe18001b3b6f01
Diego <diego.reina9@hotmail.com>
2025-03-20 08:02:54 -0500

    clase de JavaScript

---
+++ js/variables.js
@@ -0,0 +1,20 @@
+//Comentario de una linea
+/*Comentario de varias lineas
+otro comentario de varias lineas
+*/
+//Declarar variables
+var a;// se puede usar, pero las buenas practicas dictan que se deben declarar variables con let
+let b;
+const c = 5;
+
+//Variables => var, let, const
+//var => variable global
+// undefined => variable sin valor
+// null => variable sin valor
+// boolean => true, false
+// number => 1, 2, 3, 4, 5, 3.5
+// string => 'hola', "hola", `hola`
+// object => {nombre: 'Juan', apellido: 'Perez'}
+
+var x = null;
+console.log(x);
