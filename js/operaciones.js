8672396ca70fcd85cc4eebe26c0fd006858951cf
Diego <diego.reina9@hotmail.com>
2025-04-11 13:20:31 -0500

    import / export

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,16 +1,13 @@
 //Ecmascript modules: divide una aplicacion en diferentes archivos
-function add(x, y) {
-    return x + y;
-}
-
-function multiplicar(x=0, y=0) {
-    return x * y;
-}
-
+// function add(x, y) {//declaracion de funcion tradicional
+//     return x + y;
+// }
+const add = (x, y) => x + y;//funcion adaptada en Arrow Function In Line
+// function multiplicar(x=0, y=0) {//Declaracion de funcion tradicional
+//     return x * y;
+// }
+const multiplicar = (x = 0, y = 0) => x * y;//funcion adaptada en Arrow Function In Line
 let texto = 'Esto es un texto';
-
 let userDate = {nombre: 'Homero', apellido: 'Simpson', direccion: 'Av. siempre viva'};
-
 let frutas = ['mango', 'manzana', 'durazno'];
-
-export { add, multiplicar, texto, userDate, frutas };
+export { add, multiplicar, texto, userDate, frutas };//Se puede usar la palabra reservada export antes de cada funcion o especificar las funciones a exportar en un sola linea
